1c22d58b68fa28580cae09ad22ca96bb3d6a036d
Maxwell James <maxichanj@gmail.com>
2023-08-28 01:35:14 +0100

    completed the crud operation and connected to database

---
+++ routes/contactRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const {getContacts} = require("../controllers/contactController");
+const {getContact} = require("../controllers/contactController");
+const {createContact} = require("../controllers/contactController");
+const {updateContact} = require("../controllers/contactController");
+const {deleteContact} = require("../controllers/contactController");
+
+router.route('/').get(getContacts);
+
+router.route('/:id').get(getContact);
+
+
+router.route('/').post(createContact);
+
+
+router.route('/:id').put(updateContact);
+
+
+router.route('/:id').delete(deleteContact);
+
+
+
+module.exports = router;
